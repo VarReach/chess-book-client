0be5b71ffd257358a0292357d10bfad413825d44
AReach <DerAReich@gmail.com>
2019-03-08 12:38:22 -0600

    initil commit

---
+++ src/services/auth-api-service.js
@@ -0,0 +1,79 @@
+import config from '../config';
+import TokenService from './token-service';
+import IdleService from './idle-service';
+
+const AuthApiService = {
+  postLogin(credentials) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    };
+    return fetch(`${config.API_ENDPOINT}/auth/login`, options)
+      .then(res => {
+        return (!res.ok) 
+          ? res.json().then(e => Promise.reject(e))
+          : res.json();
+      })
+      .then(res => {
+        TokenService.saveAuthToken(res.authToken);
+        IdleService.registerIdleTimerResets();
+        TokenService.queueCallbackBeforeExpiry(() => {
+          AuthApiService.postRefreshToken();
+        });
+        return res;
+      })
+  },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user),
+    })
+      .then(resp => {
+        return (!resp.ok)
+          ? resp.json().then(e => Promise.reject(e))
+          : resp.json();
+      });
+  },
+  postRefreshToken() {
+    return fetch(`${config.API_ENDPOINT}/auth/refresh`, {
+      method: 'POST',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res => {
+        return (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      })
+      .then(res => {
+        /*
+          similar logic to whenever a user logs in, the only differences are:
+          - we don't need to queue the idle timers again as the user is already logged in.
+          - we'll catch the error here as this refresh is happening behind the scenes
+        */
+        TokenService.saveAuthToken(res.authToken)
+        TokenService.queueCallbackBeforeExpiry(() => {
+          AuthApiService.postRefreshToken()
+        })
+        return res
+      })
+      .catch(err => {
+        console.log('refresh token request error')
+        console.error(err)
+        //if there's an error refreshing, it logs you out, and refreshes the page.. need better solution.
+        // TokenService.clearAuthToken();
+        // TokenService.clearCallbackBeforeExpiry();
+        // IdleService.unRegisterIdleResets();
+        // window.location.reload();
+      })
+  },
+}
+
+export default AuthApiService;
