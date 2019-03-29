fe72425677ab1d76838e3e2eb147a25063a9d6f8
AReach <DerAReich@gmail.com>
2019-03-29 19:11:37 +0000

    added bug fixes for chapter preview text going off the boundaries. now uses ellipsis

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://stormy-ravine-75332.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',//'https://stormy-ravine-75332.herokuapp.com/api', //http://localhost:8000/api
   TOKEN_KEY: 'Chess-Book-Auth-Token',
-  VISIT_STATUS: 'Chess-Book-Landing-Info'
 }
